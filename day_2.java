67dd0404065469f143e353739c30a8ff8f9b3bca
NandanwarKrish2008 <155374128+NandanwarKrish2008@users.noreply.github.com>
2024-05-08 19:43:55 +0530

    Add files via upload

---
+++ day_2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//Star Pattern
+public class day_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows : ");
+        int r = sc.nextInt();
+
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println(); //This is for row spacing and it is compulsary
+        }
+    }
+}
